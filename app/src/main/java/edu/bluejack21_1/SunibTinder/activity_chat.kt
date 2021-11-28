d7d188087116fed75bbc757180982321936756e6
mavericktherry <48401892+mavericktherry@users.noreply.github.com>
2021-11-29 02:22:17 +0700

    early-navbar3

--- app/src/main/java/edu/bluejack21_1/SunibTinder/activity_chat.kt
+++ app/src/main/java/edu/bluejack21_1/SunibTinder/activity_chat.kt
@@ -1,12 +1,11 @@
 package edu.bluejack21_1.SunibTinder
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class Chat : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?){
+class activity_chat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
-
     }
 }
--- app/src/main/java/edu/bluejack21_1/SunibTinder/activity_profile.kt
+++ app/src/main/java/edu/bluejack21_1/SunibTinder/activity_profile.kt
@@ -1,12 +1,11 @@
 package edu.bluejack21_1.SunibTinder
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class Profile : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?){
+class activity_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
     }
 }
