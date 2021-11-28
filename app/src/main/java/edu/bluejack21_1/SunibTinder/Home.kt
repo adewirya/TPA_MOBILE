80de74b38ca07e970ae23bdf2bb9657f8a3e4445
mavericktherry <48401892+mavericktherry@users.noreply.github.com>
2021-11-29 04:53:25 +0700

    navbar v.final

--- app/src/main/java/edu/bluejack21_1/SunibTinder/Home.kt
+++ app/src/main/java/edu/bluejack21_1/SunibTinder/Home.kt
@@ -3,6 +3,7 @@ package edu.bluejack21_1.SunibTinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.bluejack21_1.SunibTinder.databinding.ActivityHomeBinding
@@ -15,6 +16,8 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+////        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+////        val navController = navHostFragment.navController
         val navController = findNavController(R.id.nav_fragment)
         bottomNavView.setupWithNavController(navController)
     }
