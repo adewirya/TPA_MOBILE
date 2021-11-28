06612e258c3c0bd8d790122f236e7ce70c47d880
mavericktherry <48401892+mavericktherry@users.noreply.github.com>
2021-11-29 03:07:53 +0700

    navbar-v.4

--- app/src/main/java/edu/bluejack21_1/SunibTinder/Home.kt
+++ app/src/main/java/edu/bluejack21_1/SunibTinder/Home.kt
@@ -2,11 +2,21 @@ package edu.bluejack21_1.SunibTinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import edu.bluejack21_1.SunibTinder.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavView.setupWithNavController(navController)
     }
 
 }
